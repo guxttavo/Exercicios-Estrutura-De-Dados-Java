d1f16a4698dc8e1962f94eed19e72890e8172cdd
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-08-10 09:39:36 -0300

    Initial commit

---
+++ src/Ex01.java
@@ -0,0 +1,11 @@
+/*
+Lê 5 nomes e notas de uma turma, calcula e exibe a média das notas da turma e em seguida
+exibe a relação de nomes cuja nota é superior a esta média
+ */
+
+
+public class Ex01 {
+    public static void main(String[] args) {
+
+    }
+}

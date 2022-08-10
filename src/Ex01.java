e6b17cb1097bcece10df7c098faaaf72ef420b8e
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-08-10 10:34:49 -0300

    exercicio 01

--- src/Ex01.java
+++ src/Ex01.java
@@ -1,11 +1,38 @@
 /*
-Lê 5 nomes e notas de uma turma, calcula e exibe a média das notas da turma e em seguida
-exibe a relação de nomes cuja nota é superior a esta média
+-->Criar um vetor A com 5 elementos inteiros.
+-->Construir um vetor B de mesmo tipo e tamanho e com os mesmos
+elementos do vetor A, ou seja, B[i] = A[i]
  */
 
+import java.util.Scanner;
 
 public class Ex01 {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int[] vetorA = new int[5];
+        int[] vetorB = new int[vetorA.length];
+
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.println("Entre com um valor da posicao: " + i);
+            vetorA[i] = scan.nextInt();
+
+            vetorB[i] = vetorA[i];
+        }
+
+        System.out.println("Vetor A = ");
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.println(vetorA[i] + "");
+        }
+
+        System.out.println();
+
+        System.out.println("Vetor B = ");
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.println(vetorA[i] + "");
+        }
+
 
     }
 }

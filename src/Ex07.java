6a387dbb76a1d282617b8d1ead27c3f0d82d6823
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-08-12 23:11:19 -0300

    exercicio 06 e 07 error

--- src/Ex07.java
+++ src/Ex07.java
@@ -1,24 +1,22 @@
 /*
 -->Criar dois vetores A e B cada um com 10 elementos inteiros.
--->Construir um vetor C, onde cada elemento de C é a soma dos
-   respectivos elementos em A e B, ou seja,
-   C[i]= A[i] + B[i]
+-->Construir um vetor C, onde cada elemento de C é a subtração dos respectivos
+   elementos em A e B, ou seja,
+   C[i] = A[i] + B[i]
 
-*/
-
+ */
 
 import java.util.Scanner;
 
-public class Ex06 {
+public class Ex07 {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        int[] vetorA = new int[10];
+        int[] vetorA = new int[2];
         int[] vetorB = new int[vetorA.length];
         int[] vetorC = new int[vetorA.length];
-
 
         for (int i = 0; i < vetorA.length; i++) {
             System.out.println("Entre com o valor do vetor A da posicao: " + i);
@@ -28,6 +26,10 @@ public class Ex06 {
         for (int i = 0; i < vetorB.length; i++) {
             System.out.println("Entre com o valor do vetor B da posicao: " + i);
             vetorB[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < vetorC.length; i++) {
+            vetorC[i] = vetorA[i] - vetorB[i];
         }
 
         System.out.println("Vetor A = ");
@@ -43,7 +45,7 @@ public class Ex06 {
         System.out.println("Vetor C = ");
         for (int i = 0; i < vetorC.length; i++) {
             System.out.println(vetorC[i] + " ");
-
         }
+
     }
 }
